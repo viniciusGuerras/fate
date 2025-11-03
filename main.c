61ab171f897d4fafe82ef0d6721efa7d3a52512d
Vinicius Guerra <itsviniciusguerra@gmail.com>
2025-11-03 14:17:44 -0300

    chore: removed unused files and texts

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main() {
 	size_t shape_1[3] = {1, 2, 4};
 	size_t shape_2[2] = {2, 4};
 	xoshiro_seed(time(NULL));
-	Tensor* t_1 = tensor_create(shape_1, 3, 0, DT_FLOAT);
+	Tensor* t_1 = tensor_create(shape_1, 3t:!;, 0, DT_FLOAT);
 	Tensor* t_2 = tensor_create(shape_2, 2, 0, DT_FLOAT);
 	tensor_fill_random(t_1);
 	tensor_fill_random(t_2);
